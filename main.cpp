e063c1440573d8ffe389d44473d232f235b1f3c0
加贺加贺岬 <mengsiming77@gmail.com>
2021-09-30 18:01:11 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "acllib.h"
+
+int Setup(){
+    initWindow("My Window", DEFAULT, DEFAULT, 640, 480);
+    beginPaint();
+    setTextSize(100);
+    paintText(100, 100, "Hi~");
+    endPaint();
+    return 0;
+}
